c96cc088e39eaa7b109935b2960697f34b578484
Ivo Batistela <irbatistela@gmail.com>
2016-08-04 03:05:32 -0300

    Item owned an validation impl and update techniquees

--- src/main/java/rocks/byivo/sales/model/Item.java
+++ src/main/java/rocks/byivo/sales/model/Item.java
@@ -5,6 +5,9 @@
  */
 package rocks.byivo.sales.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author byivo
@@ -21,10 +24,67 @@ public class Item extends Entity {
     public Item() {
         price = 0d;
     }
-    
-     public Item(Integer id) {
+
+    public Item(Integer id) {
         this.id = id;
         price = 0d;
+    }
+
+    private Item createMock(Item newValues) {
+        Item mockValues = new Item();
+        mockValues.setId(this.id);
+        mockValues.setPrice(newValues.getPrice());
+        mockValues.setName(newValues.getName());
+        mockValues.setImage(newValues.getImage());
+
+        return mockValues;
+    }
+
+    public Map<String, String> update(Item newValues) {
+        Item mockValues = createMock(newValues);
+
+        boolean valid = mockValues.validContent();
+
+        if (valid) {
+            this.setName(newValues.name);
+            this.setImage(newValues.getImage());
+            this.setPrice(newValues.getPrice());
+            return new HashMap<>();
+        }
+
+        return mockValues.getInvalidMessages(true);
+    }
+
+    @Override
+    public boolean validContent() {
+        this.clearInvalidMessages();
+
+        boolean invalidName = false;
+        boolean invalidImage = false;
+        boolean invalidPrice = false;
+
+        //NAME VALIDATION
+        if (invalidName |= name == null) {
+            this.putInvalidMessage("name", ModelValidation.Item.EMPTY_NAME);
+        } else if (invalidName |= name.isEmpty()) {
+            this.putInvalidMessage("name", ModelValidation.Item.EMPTY_NAME);
+        } else if (invalidName |= name.length() > 80) {
+            this.putInvalidMessage("name", ModelValidation.Item.LONGEST_NAME);
+        }
+
+        //IMAGE VALIDATION
+        if (image != null) {
+            if (invalidImage |= image.length() > 512) {
+                this.putInvalidMessage("image", ModelValidation.Item.LONGEST_IMAGE);
+            }
+        }
+
+        //PASSWORD VALIDATION
+        if (invalidPrice |= price <= 0) {
+            this.putInvalidMessage("price", ModelValidation.Item.WRONG_PRICE);
+        }
+
+        return !(invalidImage || invalidPrice || invalidName);
     }
 
     @Override
@@ -56,7 +116,6 @@ public class Item extends Entity {
     public Double getPrice() {
         return price;
     }
-
 
     public void setPrice(Double price) {
         this.price = price;
